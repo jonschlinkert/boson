1ff6eac6aa9e24bbd65742308e5d5583af901b33
jonschlinkert <dev@sellside.com>
2014-04-13 17:39:23 -0400

    split out local to normalize and expand before trying to require/register

--- index.js
+++ index.js
@@ -8,73 +8,168 @@
 
 const path = require('path');
 const file = require('fs-utils');
+const log = require('verbalize');
 const _ = require('lodash');
-const utils = require('./lib/utils');
 
-function tryRequiring(item, options) {
-  var obj = {};
-  var module = require(item);
-  if (_.isFunction(module)) {
-    _.extend(obj, module(options));
-  } else {
-    _.extend(obj, module);
-  }
-  return obj;
+const boson = module.exports = {};
+
+var toString = Object.prototype.toString;
+var type = function(val) {
+  return toString.call(val).toLowerCase().replace(/\[object ([\S]+)\]/, '$1');
 };
 
-function tryRegistering(arr, options) {
-  var obj = {};
-  file.expand(arr).map(function (filepath) {
-    var module = require(path.resolve(filepath));
+/**
+ * Load npm modules from a normalized config
+ * @param   {Object}  mod
+ * @param   {Object}  params
+ * @return  {Object}
+ */
 
+boson.loadNpm = function(mod, params) {
+  var config = params.config || {};
+  var resolved = {}, unresolved = [];
+  try {
+    _.extend(resolved, require(mod)(config));
+  } catch (err) {
     try {
-      return _.extend(obj, module(options));
-    } catch (e) {
-      return _.extend(obj, module);
+      _.extend(resolved, require(mod));
+    } catch(err) {
+      unresolved = unresolved.concat(mod);
     }
-  });
-  return obj;
+  }
+  return {
+    resolved: resolved,
+    unresolved: unresolved,
+  };
 };
 
-module.exports = function (config, options) {
-  options = options || {};
+/**
+ * Load local modules from a normalized config
+ * @param   {Object}  filepath
+ * @param   {Object}  params
+ * @return  {Object}
+ */
 
-  var context = {};
-
-  // First, coerce the config into a flattened array
-  config = utils.arrayify(config);
-
-  // Next, iterate over each item in the array
-  config.forEach(function (arr) {
-
-    // If config arr is defined as an object, extend the context directly
-    if (_.isPlainObject(arr)) {
-      _.extend(context, arr);
-    } else {
-
-      file.expand(arr).map(function (filepath) {
-        // console.log(filepath);
-      });
-
-      // Otherwise, if arr is defined as a string...
-      try {
-        // Try to require the arr as a node module
-        _.extend(context, tryRequiring(arr, options));
-      } catch (e) {
-        // If it's not a requireable module, we'll assume
-        // it's either a filepath or glob pattern and try
-        // to expand it then try to require it.
-        try {
-          _.extend(context, tryRegistering(arr, options));
-        } catch (e) {
-          // e.origin = __filename;
-          // e.message = 'Module could not be registered: (error code "' + e.code + '").';
-          // throw new Error(e);
-        }
-      }
+boson.loadLocal = function(filepath, params) {
+  var config = params.config || {};
+  var resolved = {}, unresolved = [];
+  try {
+    _.extend(resolved, require(path.resolve(filepath))(config));
+  } catch (err) {
+    try {
+      _.extend(resolved, require(path.resolve(filepath)));
+    } catch (err) {
+      unresolved = unresolved.concat(filepath);
     }
+  }
+  return {
+    resolved: resolved,
+    unresolved: unresolved,
+  };
+};
 
+
+/**
+ * Normalize strings to objects
+ * @param   {String}  patterns
+ * @return  {Object}
+ */
+
+boson.normalizeString = function(patterns) {
+  return {__patterns__: [patterns]};
+};
+
+/**
+ * Organize items in an array to strings and objects
+ * @param   {String}  patterns
+ * @return  {Object}
+ */
+
+boson.normalizeArray = function(arr, options) {
+  var patterns = [];
+  arr.forEach(function(pattern) {
+    if (type(pattern) === 'string') {
+      patterns = patterns.concat({__patterns__: [pattern]});
+    } else {
+      return boson.normalize(pattern, options);
+    }
   });
+  return patterns;
+};
 
-  return context;
+
+boson.normalize = function(value, options) {
+  options = options || {};
+  var config = _.cloneDeep(value);
+  var fn = [];
+
+  if (type(config) === 'string') {
+    fn = fn.concat(boson.normalizeString(config, options));
+  } if (type(config) === 'array') {
+    fn = fn.concat(boson.normalizeArray(config, options));
+  } if (type(config) === 'object') {
+    fn = fn.concat(config);
+  }
+  return fn;
+};
+
+
+/**
+ * Expand filepaths in the `__patterns__` property
+ * on normalized objects.
+ *
+ * @param   {Object}  config
+ * @param   {Object}  options
+ * @return  {Object}
+ */
+
+boson.expand = function(config, options) {
+  config = boson.normalize(config, options);
+  var local = [], npm = [];
+
+  config.forEach(function(pattern) {
+    if ('__patterns__' in pattern) {
+      var files = file.expand(pattern.__patterns__, options);
+
+      if (files.length > 0) {
+        local = local.concat(files);
+      } else {
+        npm = npm.concat(pattern.__patterns__);
+      }
+      delete pattern.__patterns__;
+    }
+  });
+  return {local: local, npm: npm};
+};
+
+
+boson.register = function (config, options) {
+  config = boson.expand(config, options);
+  options = options || {};
+  var resolved = {}, unresolved = [];
+
+  if (config.local.length > 0) {
+    log.verbose.warn('[local]:', config);
+
+    config.local.forEach(function(filepath) {
+      var local = boson.loadLocal(filepath, options);
+      unresolved = unresolved.concat(local.unresolved);
+      _.extend(resolved, local.resolved);
+    });
+  }
+
+  if (config.npm.length > 0) {
+    log.verbose.success('[npm]:', config);
+
+    config.npm.forEach(function(name) {
+      var npm = boson.loadNpm(name, options);
+      unresolved = unresolved.concat(npm.unresolved);
+      _.extend(resolved, npm.resolved);
+    });
+  }
+
+  return {
+    resolved: resolved,
+    unresolved: unresolved
+  };
 };
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 
+var log = require('verbalize');
 var boson = require('../');
 
 var config = {
@@ -18,7 +19,7 @@ var config = {
   b: 'test/fixtures/*.js',
   // Arrays of glob patterns
   c: ['test/fixtures/*.js'],
-  d: ['test/fixtures/*-foo.js', 'test/fixtures/*-bar.js', 'test/fixtures/*-baz.js'],
+  d: ['test/fixtures/*-foo.js', 'test/fixtures/*-bar.js', 'flip', 'test/fixtures/*-baz.js'],
 
   // node-module as string
   e: 'node-foo',
@@ -37,17 +38,58 @@ var config = {
     three: function(str) {
       return str + 'three';
     }
-  }, 'node-foo', 'node-bar', 'node-baz'],
+  }, 'node-foo', 'node-bar', 'node-baz', 'fez'],
 
-  h: ['test/fixtures/plugin.js']
+  h: ['test/fixtures/plugin.js'],
+  z: 'flim-flam'
 };
 
-console.log('config.a: ', boson(config.a));
-console.log('config.b: ', boson(config.b));
-console.log('config.c: ', boson(config.c));
-console.log('config.d: ', boson(config.d));
-console.log('config.e: ', boson(config.e));
-console.log('config.f: ', boson(config.f));
-console.log('config.g: ', boson(config.g));
-console.log('config.h: ', boson(config.h));
-// console.log(boson([['test/fixtures/*.js'], config.a, 'node-foo', config.g]));
+// console.log('config.a: ', boson.register(config.a));
+// console.log('config.b: ', boson.register(config.b));
+// console.log('config.c: ', boson.register(config.c));
+// console.log('config.d: ', boson.register(config.d));
+// console.log('config.e: ', boson.register(config.e));
+// console.log('config.f: ', boson.register(config.f));
+// console.log('config.g: ', boson.register(config.g));
+// console.log('config.h: ', boson.register(config.h));
+
+// console.log(boson.register([['test/fixtures/*.js'], config.a, 'node-foo', config.g]));
+
+
+// console.log('config.a: ', boson.normalize(config.a));
+// console.log('config.b: ', boson.normalize(config.b));
+// console.log('config.c: ', boson.normalize(config.c));
+// console.log('config.d: ', boson.normalize(config.d));
+// console.log('config.e: ', boson.normalize(config.e));
+// console.log('config.f: ', boson.normalize(config.f));
+// console.log('config.g: ', boson.normalize(config.g));
+// console.log('config.h: ', boson.normalize(config.h));
+
+// boson.normalize(config.a);
+// boson.normalize(config.b);
+// boson.normalize(config.c);
+// boson.normalize(config.d);
+// boson.normalize(config.e);
+// boson.normalize(config.f);
+// boson.normalize(config.g);
+// boson.normalize(config.h);
+
+
+// boson.expand(config.a);
+// boson.expand(config.b);
+// boson.expand(config.c);
+// boson.expand(config.d);
+// boson.expand(config.e);
+// boson.expand(config.f);
+// boson.expand(config.g);
+// boson.expand(config.h);
+
+log.success('[boson]:', boson.register(config.a));
+log.success('[boson]:', boson.register(config.b));
+log.success('[boson]:', boson.register(config.c));
+log.success('[boson]:', boson.register(config.d));
+log.success('[boson]:', boson.register(config.e));
+log.success('[boson]:', boson.register(config.f));
+log.success('[boson]:', boson.register(config.g));
+log.success('[boson]:', boson.register(config.h));
+log.success('[boson]:', boson.register(config.z));
